01eef4261ae8174a8d4f84d9acd911355832b25c
Malaravan <malaravan.mmarimuthu@gmail.com>
2026-06-10 13:04:00 +0530

    Code changes to setup the base code for EF migration with my sql

---
+++ Data/Migrations/20260610072643_employee-table-chnages.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class employeetablechnages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Organizations_OrganizationID",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_OrganizationID",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Department",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Designation",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "HiEmployee",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "OrgId",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "OrganizationID",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Employees",
+                type: "nvarchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Employees",
+                type: "nvarchar(255)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Department",
+                table: "Employees",
+                type: "nvarchar(255)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Designation",
+                table: "Employees",
+                type: "nvarchar(255)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "HiEmployee",
+                table: "Employees",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "OrgId",
+                table: "Employees",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "OrganizationID",
+                table: "Employees",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_OrganizationID",
+                table: "Employees",
+                column: "OrganizationID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Organizations_OrganizationID",
+                table: "Employees",
+                column: "OrganizationID",
+                principalTable: "Organizations",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
