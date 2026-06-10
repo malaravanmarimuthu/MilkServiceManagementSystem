0f4d6fef4b0c8658cbbb95a4714100106875dd7b
Malaravan <malaravan.mmarimuthu@gmail.com>
2026-06-10 14:08:13 +0530

    Password issues solved

--- Models/Requests/RegisterDto.cs
+++ Models/Requests/RegisterDto.cs
@@ -8,8 +8,6 @@
         public string? Password { get; set; }
         public string? EmailId { get; set; }
         public string? Mobile { get; set; }
-        public string? Department { get; set; }
-        public string? Designation { get; set; }
         public long OrgId { get; set; }
     }
 }
--- milkmanagementsystem.client/src/config.ts
+++ milkmanagementsystem.client/src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: "https://localhost:44387/api",
+    API_URL: "https://localhost:44343/api",
 };
 
 export default config;
