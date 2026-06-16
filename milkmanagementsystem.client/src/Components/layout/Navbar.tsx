ad371fdb20b9de88320da73896320731db4b91da
Malaravan <malaravan.mmarimuthu@gmail.com>
2026-06-16 16:30:20 +0530

    role and navbar issue fixed

--- milkmanagementsystem.client/src/Components/layout/Navbar.tsx
+++ milkmanagementsystem.client/src/Components/layout/Navbar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NavLink, useLocation } from "react-router-dom";
 import { useState } from "react";
 import ConfirmModal from "../Common/ConfirmModal";
