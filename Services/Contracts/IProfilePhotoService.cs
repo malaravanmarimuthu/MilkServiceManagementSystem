e70e1f2994ea5db0a839c24c632089fdd4f9d38c
Anbarasan S <anbarasansubaramanian@gmail.com>
2026-07-02 16:12:43 +0530

    ProfilePhoto issue resolved

---
+++ Services/Contracts/IProfilePhotoService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Contracts
+{
+    public interface IProfilePhotoService
+    {
+        Task<string> UploadAsync(int employeeId, IFormFile file);
+        Task<string> UpdateAsync(int employeeId, IFormFile file); 
+        Task<string?> GetPhotoUrlAsync(int employeeId);
+        Task DeleteAsync(int employeeId);
+    }
+}
