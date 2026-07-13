b8e29f436bc347e6a7a97f3964c735c44c789089
Anbarasan S <anbarasansubaramanian@gmail.com>
2026-07-13 11:43:40 +0530

    ProcurementRate And ProcurementEntry Backend Done

---
+++ Data/Entities/ProcurementEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class ProcurementEntry
+    {
+        public int ID { get; set; }
+        public long EmployeeID { get; set; }
+        public string MilkType { get; set; } = string.Empty;
+        public decimal QuantityLiters { get; set; }
+        public decimal RatePerLiter { get; set; }
+        public decimal TotalAmount { get; set; }
+        public Employee? Employee { get; set; }
+    }
+}
--- Data/Migrations/20260710103123_ADDProcurementRateTable.Designer.cs
+++ Data/Migrations/20260710103123_ADDProcurementRateTable.Designer.cs
@@ -4,6 +4,7 @@ using Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data.Migrations
 {
     [DbContext(typeof(AuthDbContext))]
-    partial class AuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260710103123_ADDProcurementRateTable")]
+    partial class ADDProcurementRateTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -373,7 +376,10 @@ namespace Data.Migrations
 
                     MySqlPropertyBuilderExtensions.UseMySqlIdentityColumn(b.Property<int>("ID"));
 
-                    b.Property<long>("EmployeeID")
+                    b.Property<int>("EmployeeID")
+                        .HasColumnType("int");
+
+                    b.Property<long?>("EmployeeID1")
                         .HasColumnType("bigint");
 
                     b.Property<string>("MilkType")
@@ -391,7 +397,7 @@ namespace Data.Migrations
 
                     b.HasKey("ID");
 
-                    b.HasIndex("EmployeeID");
+                    b.HasIndex("EmployeeID1");
 
                     b.ToTable("ProcurementEntries");
                 });
@@ -588,9 +594,7 @@ namespace Data.Migrations
                 {
                     b.HasOne("Data.Entities.Employee", "Employee")
                         .WithMany()
-                        .HasForeignKey("EmployeeID")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
+                        .HasForeignKey("EmployeeID1");
 
                     b.Navigation("Employee");
                 });
